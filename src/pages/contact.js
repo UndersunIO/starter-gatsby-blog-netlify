c0e275c775063234b07cd7e7beae230b2790d53b
UndersunIO <alex.atc.digital@gmail.com>
2022-01-30 02:24:57 +0100

    init setup key

---
+++ src/pages/contact.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Layout from "../components/layout";
+
+const ContactFormPage = () => (
+  <Layout>
+    <h1>Contact</h1>
+  </Layout>
+);
+
+export default ContactFormPage;
