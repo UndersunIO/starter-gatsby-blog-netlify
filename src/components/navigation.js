821020ab749613ca758efa8d0ea4ed92f7cb14ed
UndersunIO <alex.atc.digital@gmail.com>
2022-01-30 02:58:43 +0100

    init contact

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -7,7 +7,7 @@ const Navigation = () => (
   <nav role="navigation" className={styles.container} aria-label="Main">
     <Link to="/" className={styles.logoLink}>
       <span className={styles.logo} />
-      <span className={styles.navigationItem}>Gatsby Starter Contentful</span>
+      <span className={styles.navigationItem}>Nouvelle Civilisation</span>
     </Link>
     <ul className={styles.navigation}>
       <li className={styles.navigationItem}>
@@ -16,13 +16,18 @@ const Navigation = () => (
         </Link>
       </li>
       <li className={styles.navigationItem}>
+        <Link to="/a-propos/" activeClassName="active">
+          A propos
+        </Link>
+      </li>
+      <li className={styles.navigationItem}>
         <Link to="/blog/" activeClassName="active">
           Blog
         </Link>
       </li>
       <li className={styles.navigationItem}>
-        <Link to="/contact" activeClassName="active">
-          Blog
+        <Link to="/contact/" activeClassName="active">
+          Contact
         </Link>
       </li>
     </ul>
